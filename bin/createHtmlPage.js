f4ee5d5665d96c0605b66d2def8229bf1e26d120
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-04-12 17:51:23 +0200

    bunch of changes

--- bin/createHtmlPage.js
+++ bin/createHtmlPage.js
@@ -1,20 +1,26 @@
 #!/usr/bin/env node
 
+const { documentFromHtmlString } = require("./htmlOperations")
+const createRenderer = require("./renderer")
+const { getTitle, setTitle } = require("./title")
+
 const [inputFilePath, outputFilePath] = process.argv.slice(2)
 console.log({ inputFilePath, outputFilePath })
 
-const addHeader = require("./addHeader")
-
 const marked = require("marked")
+marked.use({ renderer: createRenderer(setTitle) })
 const { readFile, writeFile } = require("fs")
 
 const createHtmlPage = mdString => {
   const htmlFromMd = marked(mdString)
-  return addHeader(htmlFromMd)
+  const document = documentFromHtmlString({
+    content: htmlFromMd,
+    title: getTitle(),
+  })
+  return document.documentElement.outerHTML
 }
 
 readFile(inputFilePath, "utf-8", (_, mdString) => {
   const htmlPage = createHtmlPage(mdString)
-  console.log({ outputFilePath, htmlPage })
   writeFile(outputFilePath, htmlPage, () => {})
 })
