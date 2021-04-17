537a103056aad378e74e1c1a37103d987199d149
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-04-17 15:05:14 +0200

    update html

--- bin/htmlOperations.js
+++ bin/htmlOperations.js
@@ -12,41 +12,76 @@ const createHtmlFrame = ({ content, title }) =>
     </title>
     <style>
       header {
-        height: 8rem;
+        height: 6rem;
         display: flex;
-        aligh-items: center;
-        justify-content: space-between;
+        align-items: center;
+        justify-content: flex-end;
+        margin: 0 8rem;
       }
 
-      header nav>ul {
-        list-style: none;
+      header * {
+        margin-right: 2rem;
       }
 
-      header li {
-        margin: 2rem;
+      header *:last-child {
+        margin-right: 0;
       }
 
-      header li>a {
-        text-decoration: none
+      nav {
+        padding: 1rem;
+        display: flex;
+        flex: 8;
+      }
+
+      header nav>a {
+        text-decoration: none;
+        margin-right: 2rem;
+      }
+
+      header section>a {
+        text-decoration: none;
+        margin: 0.5rem;
+      }
+
+      .social {
+        flex: 2;
+        display: flex;
+        justify-content: flex-end;
+        margin-left: 2rem;
+      }
+
+      main {
+        max-width: 1080px;
+        margin: 0 auto !important;
+        float: none !important;
+      }
+
+      main li {
+        margin-top: 0.5rem;
       }
     </style>
   </head>
   <body>
   <header>
+    <strong>
+      Pravin Bashyal
+    </strong>
     <nav>
-      <ul>
-        <li>
-          <a href="/">
-            About Me
-          </a>
-        </li>
-        <li>
-          <a href="/blogs">
-            Blogs
-          </a>
-        </li>
-      </ul>
+      <a href="/">
+        About Me
+      </a>
+      <a href="/blogs">
+        Blogs
+      </a>
     </nav>
+    <section class="social" aria-label="social">
+      <a href="https://twitter.com">
+        Twitter
+      </a>
+      <a href="https://twitter.com">
+        Github
+      </a>
+    </section>
   </header>
   <main>
     <article>
