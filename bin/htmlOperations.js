bdef4a5df9497a4d020093ab77ab08d2eb51749a
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-04-17 10:28:41 +0200

    add meta viewport

--- bin/htmlOperations.js
+++ bin/htmlOperations.js
@@ -5,6 +5,7 @@ const createHtmlFrame = ({ content, title }) =>
   <!DOCTYPE html>
   <html lang="en-US">
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
     ${title}
     </title>
