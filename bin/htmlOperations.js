d4f31654cd889f90d8876f196e48f08fc878a7cb
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-04-19 18:08:26 +0200

    secure shell

--- bin/htmlOperations.js
+++ bin/htmlOperations.js
@@ -12,7 +12,7 @@ const createHtmlFrame = ({ content, title, fileMeta }) =>
     </title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="//pravinbashyal.com/styles.css">
+    <link rel="stylesheet" href="https://pravinbashyal.com/styles.css">
   </head>
   <body>
   <header>
